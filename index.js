4870fa66bac3d79438770e90c58b9eadcf5378c0
Rikard Almroth <rikard.almroth@epicalgroup.com>
2024-09-11 12:29:20 +0200

    try-catch added

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
-// https://www.youtube.com/watch?v=v-fV5hAz8xE
 const express = require('express');
 const app = express();
 const port = 8080;
 const fs = require('fs');
 
-var user_value = fs.readFileSync("/etc/secret-volume/user", 'utf8');
-var pwd_value = fs.readFileSync("/etc/secret-volume/password", 'utf8');
+try {
+    var user_value = fs.readFileSync("/etc/secret-volume/user", 'utf8');
+} catch (err) {
+    user_value = err;
+}
 
-//let user_value = 'my_user';
-//let pwd_value = 'my_pwd';
+try {
+    var pwd_value = fs.readFileSync("/etc/secret-volume/password", 'utf8');
+} catch (err) {
+    pwd_value = err;
+}
 
 app.get('/', function (req, res) {
-    res.send(`<H1>Picked up from kubernetes secrets</H1> <p> The secret username: ${user_value} <p> The secret password: ${pwd_value}`);
+    res.send(`<H1>Picked up from kubernetes secrets, mounted as a volume</H1> <p> The secret username: ${user_value} <p> The secret password: ${pwd_value}`);
 });
 
 app.listen(port, function () {
